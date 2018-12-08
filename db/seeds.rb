1f5f3f44a7bcd36fa17160c3893ad36d46673609
ellfiscina <ellfiscina@gmail.com>
2018-12-08 20:35:13 -0300

    adiciona seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,88 @@
-[:N, :P, :K, :Ca, :Mg, :S, :B, :Cu, :Fe, :Mn, :Zn]
+Element.create!(
+  name: 'N',
+  slope: 0.146981810597045,
+  intersection: -57.7374191980423,
+  error: 0.00145251204535,
+  upper_limit: 43.8495765121557,
+  inferior_limit: 40.162287894624
+)
+Element.create!(
+  name: 'P',
+  slope: 0.066060198040454,
+  intersection: -40.4392916804013,
+  error: 0.000484131921255,
+  upper_limit: 5.21635549948857,
+  inferior_limit: 3.56330551746058
+)
+Element.create!(
+  name: 'K',
+  slope: 0.145777905657953,
+  intersection: -81.3424709615782,
+  error: 0.00542598943722,
+  upper_limit: 19.5428344518686,
+  inferior_limit: 15.6249621583008
+)
+Element.create!(
+  name: 'Ca',
+  slope: -0.020871534431737,
+  intersection: 28.8678728064049,
+  error: 0.010263518521533,
+  upper_limit: 16.1073713910404,
+  inferior_limit: 13.3011031852308
+)
+Element.create!(
+  name: 'Mg',
+  slope: 0.111530725695162,
+  intersection: -60.9774100912356,
+  error: 0.001336962619634,
+  upper_limit: 16.1111323874445,
+  inferior_limit: 13.3058167650979
+)
+Element.create!(
+  name: 'S',
+  slope: 0.111530725695162,
+  intersection: -60.9774100912356,
+  error: 0.001336962619634,
+  upper_limit: 16.1111323874445,
+  inferior_limit: 13.3058167650979
+)
+Element.create!(
+  name: 'B',
+  slope: 0.655708049560396,
+  intersection: -409.438794716935,
+  error: 0.004349224998861,
+  upper_limit: 43.7307795011918,
+  inferior_limit: 27.331932363215
+)
+Element.create!(
+  name: 'Cu',
+  slope: 0.160304472764316,
+  intersection: -96.4783132294523,
+  error: 0.001598887748577,
+  upper_limit: 14.3168975442652,
+  inferior_limit: 10.2949668625145
+)
+Element.create!(
+  name: 'Fe',
+  slope: 0.313147696015167,
+  intersection: -83.2348720856822,
+  error: 0.00242186354309,
+  upper_limit: 133.189725585199,
+  inferior_limit: 125.350952380902
+)
+Element.create!(
+  name: 'Mn',
+  slope: 3.1500997279132,
+  intersection: -1860.13038824048,
+  error: 0.037391724365329,
+  upper_limit: 317.169985445999,
+  inferior_limit: 237.948658621798
+)
+Element.create!(
+  name: 'Zn',
+  slope: 1.2150671091569,
+  intersection: -748.201812142781,
+  error: 0.010597199166458,
+  upper_limit: 91.5772098793471,
+  inferior_limit: 61.1329596121784
+)
