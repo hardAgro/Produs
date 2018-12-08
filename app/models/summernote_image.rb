d0dda6cc2497937a4dc9f25b820a498a3df94717
Léo Santos <aleotory@gmail.com>
2018-12-08 14:57:42 -0300

    Remove Summernote

--- app/models/summernote_image.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class SummernoteImage < ApplicationRecord
-  mount_uploader :file, ImageUploader
-end
--- db/migrate/20170712115057_create_summernote_images.rb
+++
@@ -1,9 +0,0 @@
-class CreateSummernoteImages < ActiveRecord::Migration[5.0]
-  def change
-    create_table :summernote_images do |t|
-      t.string :file
-
-      t.timestamps
-    end
-  end
-end
