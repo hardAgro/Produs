371d33caa72c3f262ca19e69ff0adbf71fffd27b
Léo Santos <aleotory@gmail.com>
2018-12-09 13:36:30 -0300

    Bugfixes

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,12 +3,5 @@
 class DashboardsController < ApplicationController
   def index
     # @cobra = Python.run('produs.py')
-    @macroNutrients = this.getMacroNutrients();
   end
-
-  def getMacroNutrients()
-    @desiredNutrients = ['n', 'p', 'k', 'mg']
-    Element.all.map do |element|
-      if()
-    end
 end
