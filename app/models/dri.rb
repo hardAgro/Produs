9640737ddf11529f866cfad9b23de4e044d2e38e
Léo Santos <aleotory@gmail.com>
2018-12-09 14:18:29 -0300

    Fix relations

--- app/models/dri.rb
+++ app/models/dri.rb
@@ -3,11 +3,11 @@ class Dri < ApplicationRecord
   belongs_to :measurement
 
   def self.calculate(concentrations)
-    concentrations.each do |concentration|
+    concentrations.map do |concentration|
       values = (concentrations - [concentration]).pluck(:value)
       sum = values.sum { |x| concentration.value / x }
       inverse_sum = values.sum { |x| x / concentration.value }
-      Dri.create(value: sum - inverse_sum, element: concentration.element)
+      Dri.new(value: sum - inverse_sum, element: concentration.element)
     end
   end
 end
